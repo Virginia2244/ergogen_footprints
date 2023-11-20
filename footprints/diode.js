0747a09b32db2db2366d172925bf760ef35443b0
Carter Pollan <carter.pollan@gmail.com>
2023-11-20 00:47:53 -0700

    init of diode, it is the stock ergogen diode with the addition of traces in between the smd pads and the through holes

---
+++ footprints/diode.js
@@ -0,0 +1,89 @@
+module.exports = {
+    params: {
+        designator: 'D',
+        from: undefined,
+        to: undefined
+    },
+    body: p => {
+    /*I stole get_at_coordinates() and adjust_point() from infused-kim's guide at https://nilnil.notion.site/Convert-Kicad-Footprints-to-Ergogen-8340ce87ad554c69af4e3f92bc9a0898
+    I have no idea how it works. I am pretty sure that it interfaces with the other ergogen code in fancy ways.
+    I do know that get_at_coordinates() is a helper funciton for adjust_point*/
+    const get_at_coordinates = () => {
+    const pattern = /\(at (-?[\d\.]*) (-?[\d\.]*) (-?[\d\.]*)\)/;
+    const matches = p.at.match(pattern);
+    if (matches && matches.length == 4) {
+        return [parseFloat(matches[1]), parseFloat(matches[2]), parseFloat(matches[3])];
+    } else {
+        return null;
+    }
+    }
+
+    /*Call adjust_point if you want to make something move that is outisde of the main body of the footprint. Aka after the ')' in the return statement*/
+    const adjust_point = (x, y) => {
+    const at_l = get_at_coordinates();
+    if(at_l == null) {
+        throw new Error(
+        `Could not get x and y coordinates from p.at: ${p.at}`
+        );
+    }
+    const at_x = at_l[0];
+    const at_y = at_l[1];
+    const at_angle = at_l[2];
+    const adj_x = at_x + x;
+    const adj_y = at_y + y;
+
+    const radians = (Math.PI / 180) * at_angle,
+        cos = Math.cos(radians),
+        sin = Math.sin(radians),
+        nx = (cos * (adj_x - at_x)) + (sin * (adj_y - at_y)) + at_x,
+        ny = (cos * (adj_y - at_y)) - (sin * (adj_x - at_x)) + at_y;
+
+    const point_str = `${nx.toFixed(2)} ${ny.toFixed(2)}`;
+    return point_str;
+    }
+
+    return `
+    (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
+
+
+        ${p.at /* parametric position */}
+
+        ${'' /* footprint reference */}
+        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+        
+        ${''/* diode symbols */}
+        (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
+        (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
+        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
+        (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
+        (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
+        (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
+        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer B.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
+        (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
+    
+        ${''/* SMD pads on both sides */}
+        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.to.str})
+        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.from.str})
+        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to.str})
+        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from.str})
+        
+        ${''/* THT terminals */}
+        (pad 1 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to.str})
+        (pad 2 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.from.str})
+
+    )
+
+    (segment (start ${adjust_point(-1.65, 0)}) (end ${adjust_point(-3.81, 0)}) (width 0.25) (layer "F.Cu") (net 1))
+    (segment (start ${adjust_point(-1.65, 0)}) (end ${adjust_point(-3.81, 0)}) (width 0.25) (layer "B.Cu") (net 1))
+    (segment (start ${adjust_point( 1.65, 0)}) (end ${adjust_point( 3.81, 0)}) (width 0.25) (layer "F.Cu") (net 1))
+    (segment (start ${adjust_point( 1.65, 0)}) (end ${adjust_point( 3.81, 0)}) (width 0.25) (layer "B.Cu") (net 1))
+    `
+    }
+}
