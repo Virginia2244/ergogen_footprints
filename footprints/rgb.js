cd2cd435cd30bcfaa9de810f75eb608a94e86d78
Carter Pollan <carter.pollan@gmail.com>
2024-08-24 22:29:29 -0500

    added cutout and reversability to the rgb footprint

--- footprints/rgb.js
+++ footprints/rgb.js
@@ -9,6 +9,16 @@ module.exports = {
     GND : {type : 'net', value : 'GND'}
   },
   body : p => {
+
+    const footprint_spacing = {
+      x: 2.9,
+      y: 1.6,
+    }
+    const via_spacing = {
+      x: 2.5,
+      y: 3,
+    }
+
     const standard = `
     
     (module WS2812B (layer F.Cu) (tedit 53BEE615)
@@ -72,11 +82,6 @@ module.exports = {
     (fp_text reference '${p.ref}' (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
     (fp_text value '' (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
-    (fp_line (start -1.75 -1.75) (end -1.75 1.75) (layer F.SilkS) (width 0.15))
-    (fp_line (start -1.75 1.75) (end 1.75 1.75) (layer F.SilkS) (width 0.15))
-    (fp_line (start 1.75 1.75) (end 1.75 -1.75) (layer F.SilkS) (width 0.15))
-    (fp_line (start 1.75 -1.75) (end -1.75 -1.75) (layer F.SilkS) (width 0.15))
-
     (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer F.SilkS) (width 0.15))
     (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer F.SilkS) (width 0.15))
     (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer F.SilkS) (width 0.15))
@@ -84,47 +89,35 @@ module.exports = {
 
     (fp_poly (pts (xy 4 2.2) (xy 4 0.375) (xy 5 1.2875)) (layer F.SilkS) (width 0.1))
 
-    (pad 1 smd rect (at -2.2 -0.875 ${p.rot}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.VCC.str})
-    (pad 2 smd rect (at -2.2 0.875 ${p.rot}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.dout.str})
-    (pad 3 smd rect (at 2.2 0.875 ${p.rot}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.GND.str})
-    (pad 4 smd rect (at 2.2 -0.875 ${p.rot}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.din.str})
-
-    (pad 11 smd rect (at -2.5 -1.6 ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.VCC.str})
-    (pad 22 smd rect (at -2.5 1.6 ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.dout.str})
-    (pad 33 smd rect (at 2.5 1.6 ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.GND.str})
-    (pad 44 smd rect (at 2.5 -1.6 ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.din.str})
+    (pad 11 smd rect (at -${footprint_spacing.x} -${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.VCC.str})
+    (pad 22 smd rect (at -${footprint_spacing.x} ${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.dout.str})
+    (pad 33 smd rect (at ${footprint_spacing.x} ${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.GND.str})
+    (pad 44 smd rect (at ${footprint_spacing.x} -${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.din.str})
     
 
 
 
-
-    (fp_line (start -1.75 -1.75) (end -1.75 1.75) (layer B.SilkS) (width 0.15))
-    (fp_line (start -1.75 1.75) (end 1.75 1.75) (layer B.SilkS) (width 0.15))
-    (fp_line (start 1.75 1.75) (end 1.75 -1.75) (layer B.SilkS) (width 0.15))
-    (fp_line (start 1.75 -1.75) (end -1.75 -1.75) (layer B.SilkS) (width 0.15))
+    (fp_line (start -1.75 -1.75) (end -1.75 1.75) (layer Edge.Cuts) (width 0.15))
+    (fp_line (start -1.75 1.75) (end 1.75 1.75) (layer Edge.Cuts) (width 0.15))
+    (fp_line (start 1.75 1.75) (end 1.75 -1.75) (layer Edge.Cuts) (width 0.15))
+    (fp_line (start 1.75 -1.75) (end -1.75 -1.75) (layer Edge.Cuts) (width 0.15))
 
     (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer B.SilkS) (width 0.15))
     (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer B.SilkS) (width 0.15))
     (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer B.SilkS) (width 0.15))
     (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer B.SilkS) (width 0.15))
 
-    (fp_poly (pts (xy 4 2.2) (xy 4 0.375) (xy 5 1.2875)) (layer B.SilkS) (width 0.1))
+    (fp_poly (pts (xy -4 2.2) (xy -4 0.375) (xy -5 1.2875)) (layer B.SilkS) (width 0.1))
 
-    (pad 5 smd rect (at -2.2 -0.875 ${p.rot}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.VCC.str})
-    (pad 6 smd rect (at -2.2 0.875 ${p.rot}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.dout.str})
-    (pad 7 smd rect (at 2.2 0.875 ${p.rot}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.GND.str})
-    (pad 8 smd rect (at 2.2 -0.875 ${p.rot}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.din.str})
+    (pad 12 smd rect (at -${footprint_spacing.x} -${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.din.str})
+    (pad 23 smd rect (at -${footprint_spacing.x} ${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.GND.str})
+    (pad 34 smd rect (at ${footprint_spacing.x} ${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.dout.str})
+    (pad 45 smd rect (at ${footprint_spacing.x} -${footprint_spacing.y} ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.VCC.str})
 
-    (pad 12 smd rect (at -2.5 -1.6 ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.VCC.str})
-    (pad 23 smd rect (at -2.5 1.6 ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.dout.str})
-    (pad 34 smd rect (at 2.5 1.6 ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.GND.str})
-    (pad 45 smd rect (at 2.5 -1.6 ${p.rot}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.din.str})
-
-
-    (pad 50 thru_hole circle (at -2.5 -1.6) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.VCC.str})
-    (pad 51 thru_hole circle (at -2.5 1.6) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.dout.str})
-    (pad 52 thru_hole circle (at 2.5 1.6) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.GND.str})
-    (pad 53 thru_hole circle (at 2.5 -1.6) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.din.str})
+    (pad 50 thru_hole circle (at -${via_spacing.x} -${via_spacing.y}) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.din.str})
+    (pad 51 thru_hole circle (at -${via_spacing.x} ${via_spacing.y}) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.GND.str})
+    (pad 52 thru_hole circle (at ${via_spacing.x} ${via_spacing.y}) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.dout.str})
+    (pad 53 thru_hole circle (at ${via_spacing.x} -${via_spacing.y}) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${p.VCC.str})
       
 )
 `
